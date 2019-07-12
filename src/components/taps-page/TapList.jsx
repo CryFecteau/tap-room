e9c546f9f04138edac40c8f5824147a23fcf223d
Brendan Hellar <bwhellar@gmail.com>
2019-07-12 11:43:53 -0700

    Add style to tap list

--- src/components/taps-page/TapList.jsx
+++ src/components/taps-page/TapList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Taps from './Taps';
 import PropTypes from 'prop-types';
 import TapBanner from '../img/tap-room.png';
+import Keg from '../img/keg.png';
 import './TapsPage.css';
 
 
@@ -14,6 +15,30 @@ function TapList(props){
             <div>
                 <a href='/createTaps'>Add Taps</a>
             </div>
+            <div className="tapCon">
+                <div className="colTap">
+                    <img src={Keg}/>
+                    <h3>Sickly</h3>
+                    <p>Style: Brown Pale Ale</p>
+                    <p>7%</p>
+                    <p>Tapped 3 hours ago</p>
+                </div>
+                <div className="colTap">
+                    <img src={Keg}/>
+                    <h3>Reaper</h3>
+                    <p>Style: Stout</p>
+                    <p>9%</p>
+                    <p>Tapped 7 hours ago</p>
+                </div>
+                <div className="colTap">
+                    <img src={Keg}/>
+                    <h3>Radioactive</h3>
+                    <p>Style: Double IPA</p>
+                    <p>8.5%</p>
+                    <p>Tapped 1 hour ago</p>
+                </div>
+            </div>
+            
             <div className="taps">
                 {props.tapList.map((taps)=>
                     <Taps 
@@ -23,9 +48,6 @@ function TapList(props){
                     formattedWaitTime={taps.formattedWaitTime}
                     key={taps.id} />
                 )}
-                <div>
-                
-                </div>
             </div>
         </div>
     );
--- src/components/taps-page/Taps.jsx
+++ src/components/taps-page/Taps.jsx
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types';
 function Taps(props){
     return(
         <div>
-            <h3>{props.name}</h3>
-            <p>{props.percent}</p> 
-            <p>{props.style}</p>
-            <p>{props.formattedWaitTime} ago</p>
+            <div>
+                <h3>{props.name}</h3>
+                <p>Style: {props.style}</p>
+                <p>{props.percent}%</p> 
+                <p>Tapped {props.formattedWaitTime} ago</p>
+            </div>
         </div>
+
+       
     );
 }
 Taps.propType={

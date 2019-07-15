2b278ea5246c8eb7d8a4bc33a694dc6d2b169159
Student <example@example.com>
2019-07-15 13:38:39 -0700

    Updates

---
+++ precache-manifest.bb01f0fc34eae22c7e5888abe4f65cd0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133e3614838864b7b36f0c429f10e3ee",
+    "url": "/tap-room/index.html"
+  },
+  {
+    "revision": "2f2a7bb0c6a46ad9e6d7",
+    "url": "/tap-room/static/css/main.a42d61b7.chunk.css"
+  },
+  {
+    "revision": "4566cbcef32e0359f378",
+    "url": "/tap-room/static/js/2.69349977.chunk.js"
+  },
+  {
+    "revision": "2f2a7bb0c6a46ad9e6d7",
+    "url": "/tap-room/static/js/main.37e8ddf5.chunk.js"
+  },
+  {
+    "revision": "bc9fc6b548ef86fc646c",
+    "url": "/tap-room/static/js/runtime~main.7960e82e.js"
+  },
+  {
+    "revision": "571c2702615f2045a8399b7e14823668",
+    "url": "/tap-room/static/media/banner.571c2702.png"
+  },
+  {
+    "revision": "846063a122fa39488afcb6cb98f51cd8",
+    "url": "/tap-room/static/media/comeIn.846063a1.png"
+  },
+  {
+    "revision": "8a74da780bccbca102aa15f0573998ad",
+    "url": "/tap-room/static/media/drunkDrink.8a74da78.png"
+  },
+  {
+    "revision": "29e987056ccb38174a18897eb94a6a56",
+    "url": "/tap-room/static/media/happyHour.29e98705.png"
+  },
+  {
+    "revision": "f048d873a491ffa20de07b800196a70d",
+    "url": "/tap-room/static/media/instagram.f048d873.png"
+  },
+  {
+    "revision": "57934e1f92b82c3b60530cb6bf250dd1",
+    "url": "/tap-room/static/media/keg.57934e1f.png"
+  },
+  {
+    "revision": "a993f52909a026765cde7a6eb8a35e86",
+    "url": "/tap-room/static/media/logo.a993f529.png"
+  },
+  {
+    "revision": "cd7ab71053ef4c0d1884306ed4baf779",
+    "url": "/tap-room/static/media/tap-room.cd7ab710.png"
+  },
+  {
+    "revision": "a19c7a19f30ec930e6293dba2d5fa68d",
+    "url": "/tap-room/static/media/work.a19c7a19.png"
+  },
+  {
+    "revision": "95dc6c9b35b1ee4daefb5a3f57888313",
+    "url": "/tap-room/static/media/zombie.95dc6c9b.png"
+  },
+  {
+    "revision": "a6c001b0785c3e6a1a5f8267cf250a2b",
+    "url": "/tap-room/static/media/zombit.a6c001b0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb647b26ae30558e3c5151a112739fe6.js"
+  "/tap-room/precache-manifest.bb01f0fc34eae22c7e5888abe4f65cd0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tap-room/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
